c89c9670bd4d5f3db31a071fd566398d8e4fa647
Chanchal Malik <chanchalmalik1214@gmail.com>
2026-05-11 01:28:05 +0530

    removed .env from tracking

--- backend/ai/gemini_service.py
+++ backend/ai/gemini_service.py
@@ -20,7 +20,7 @@ def generate_summary(text):
     """
 
     response = client.models.generate_content(
-        model="gemini-2.0-flash",
+        model="gemini-3-flash-preview",
         contents=prompt
     )
 
